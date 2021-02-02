ab12892ea1065be887afa44d72fc15ccf7b4d49e
Kinson yan <kinson.yan@outlook.com>
2021-02-02 10:32:12 +0800

    优化

--- jd_blueCoin.js
+++ jd_blueCoin.js
@@ -82,13 +82,13 @@ const JD_API_HOST = `https://api.m.jd.com/api?appid=jdsupermarket`;
         }
       }
       try {
-      if (`${coinToBeans}` !== '0') {
-        await smtgHome();//查询蓝币数量，是否满足兑换的条件
-        await PrizeIndex();
-      } else {
-        console.log('查询到您设置的是不兑换京豆选项，现在为您跳过兑换京豆。如需兑换，请去BoxJs设置或者修改脚本coinToBeans\n')
-      }
-      await msgShow();
+        if (`${coinToBeans}` !== '0') {
+          await smtgHome();//查询蓝币数量，是否满足兑换的条件
+          await PrizeIndex();
+        } else {
+          console.log('查询到您设置的是不兑换京豆选项，现在为您跳过兑换京豆。如需兑换，请去BoxJs设置或者修改脚本coinToBeans\n')
+        }
+        await msgShow();
       } catch (e) {
         $.logErr(e)
       }
