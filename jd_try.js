61535071367dcdb06a8875747a53bf915db36495
Kinson yan <kinson.yan@outlook.com>
2021-03-16 15:20:06 +0800

    京东试用更新于20210316

--- jd_try.js
+++ jd_try.js
@@ -26,7 +26,7 @@ let allGoodList = []
 $.pageSize = 12
 let cidsList = ["家用电器", "手机数码", "电脑办公", "家居家装"]
 let typeList = ["普通试用", "闪电试用"]
-let goodFilters = "教程@软件@英语@辅导@培训".split('@')
+let goodFilters = "教程@软件@英语@辅导@培训@电信卡@移动卡@联通卡@上网卡@体验卡@流量卡".split('@')
 let minPrice = 0
 
 const cidsMap = {
