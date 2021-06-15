9a95ad111b42b3d51fc36f4219b93ac0f6da4596
Kinson yan <kinson.yan@outlook.com>
2021-06-15 14:07:31 +0800

    Update jd_jdzz.js

--- jd_jdzz.js
+++ jd_jdzz.js
@@ -8,24 +8,24 @@
 ============Quantumultx===============
 [task_local]
 # 京东赚赚
-10 0 * * * https://jdsharedresourcescdn.azureedge.net/jdresource/jd_jdzz.js, tag=京东赚赚, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jdzz.png, enabled=true
+10 0 * * * jd_jdzz.js, tag=京东赚赚, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jdzz.png, enabled=true
 
 ================Loon==============
 [Script]
-cron "10 0 * * *" script-path=https://jdsharedresourcescdn.azureedge.net/jdresource/jd_jdzz.js,tag=京东赚赚
+cron "10 0 * * *" script-path=jd_jdzz.js,tag=京东赚赚
 
 ===============Surge=================
-京东赚赚 = type=cron,cronexp="10 0 * * *",wake-system=1,timeout=3600,script-path=https://jdsharedresourcescdn.azureedge.net/jdresource/jd_jdzz.js
+京东赚赚 = type=cron,cronexp="10 0 * * *",wake-system=1,timeout=3600,script-path=jd_jdzz.js
 
 ============小火箭=========
-京东赚赚 = type=cron,script-path=https://jdsharedresourcescdn.azureedge.net/jdresource/jd_jdzz.js, cronexpr="10 0 * * *", timeout=3600, enable=true
+京东赚赚 = type=cron,script-path=jd_jdzz.js, cronexpr="10 0 * * *", timeout=3600, enable=true
  */
 const $ = new Env('京东赚赚');
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
-let helpAuthor=true; // 帮助作者
-const randomCount = 0; //const randomCount = $.isNode() ? 20 : 5;
+let helpAuthor=false; // 帮助作者
+const randomCount = 0;//const randomCount = $.isNode() ? 20 : 5;
 let jdNotify = true; // 是否关闭通知，false打开通知推送，true关闭通知推送
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '', message = '', allMessage = '';
@@ -44,7 +44,22 @@ const inviteCodes = [
   'SZ3_rmZ-VI8ds@S5KkcRx0c81zXc0-gxaYPIQ@S5KkcRx8epgDUJEzxlfUDcg@S5KkcRU8Y9lbecUzykvEDdg@Sv_50RR4Z9lXTIh6b1A',
   'SZ3_rmZ-VI8ds@S5KkcRx0c81zXc0-gxaYPIQ@S5KkcRx8epgDUJEzxlfUDcg@S5KkcRU8Y9lbecUzykvEDdg@Sv_50RR4Z9lXTIh6b1A',
   'SZ3_rmZ-VI8ds@S5KkcRx0c81zXc0-gxaYPIQ@S5KkcRx8epgDUJEzxlfUDcg@S5KkcRU8Y9lbecUzykvEDdg@Sv_50RR4Z9lXTIh6b1A',
-  'SZ3_rmZ-VI8ds@S5KkcRx0c81zXc0-gxaYPIQ@S5KkcRx8epgDUJEzxlfUDcg@S5KkcRU8Y9lbecUzykvEDdg@Sv_50RR4Z9lXTIh6b1A', 
+  'SZ3_rmZ-VI8ds@S5KkcRx0c81zXc0-gxaYPIQ@S5KkcRx8epgDUJEzxlfUDcg@S5KkcRU8Y9lbecUzykvEDdg@Sv_50RR4Z9lXTIh6b1A',
+  'SZ3_rmZ-VI8ds@S5KkcRx0c81zXc0-gxaYPIQ@S5KkcRx8epgDUJEzxlfUDcg@S5KkcRU8Y9lbecUzykvEDdg@Sv_50RR4Z9lXTIh6b1A',
+  'SZ3_rmZ-VI8ds@S5KkcRx0c81zXc0-gxaYPIQ@S5KkcRx8epgDUJEzxlfUDcg@S5KkcRU8Y9lbecUzykvEDdg@Sv_50RR4Z9lXTIh6b1A',
+  'SZ3_rmZ-VI8ds@S5KkcRx0c81zXc0-gxaYPIQ@S5KkcRx8epgDUJEzxlfUDcg@S5KkcRU8Y9lbecUzykvEDdg@Sv_50RR4Z9lXTIh6b1A',
+  'SZ3_rmZ-VI8ds@S5KkcRx0c81zXc0-gxaYPIQ@S5KkcRx8epgDUJEzxlfUDcg@S5KkcRU8Y9lbecUzykvEDdg@Sv_50RR4Z9lXTIh6b1A',
+  'SZ3_rmZ-VI8ds@S5KkcRx0c81zXc0-gxaYPIQ@S5KkcRx8epgDUJEzxlfUDcg@S5KkcRU8Y9lbecUzykvEDdg@Sv_50RR4Z9lXTIh6b1A',
+  'SZ3_rmZ-VI8ds@S5KkcRx0c81zXc0-gxaYPIQ@S5KkcRx8epgDUJEzxlfUDcg@S5KkcRU8Y9lbecUzykvEDdg@Sv_50RR4Z9lXTIh6b1A',
+  'SZ3_rmZ-VI8ds@S5KkcRx0c81zXc0-gxaYPIQ@S5KkcRx8epgDUJEzxlfUDcg@S5KkcRU8Y9lbecUzykvEDdg@Sv_50RR4Z9lXTIh6b1A',
+  'SZ3_rmZ-VI8ds@S5KkcRx0c81zXc0-gxaYPIQ@S5KkcRx8epgDUJEzxlfUDcg@S5KkcRU8Y9lbecUzykvEDdg@Sv_50RR4Z9lXTIh6b1A',
+  'SZ3_rmZ-VI8ds@S5KkcRx0c81zXc0-gxaYPIQ@S5KkcRx8epgDUJEzxlfUDcg@S5KkcRU8Y9lbecUzykvEDdg@Sv_50RR4Z9lXTIh6b1A',
+  'SZ3_rmZ-VI8ds@S5KkcRx0c81zXc0-gxaYPIQ@S5KkcRx8epgDUJEzxlfUDcg@S5KkcRU8Y9lbecUzykvEDdg@Sv_50RR4Z9lXTIh6b1A',
+  'SZ3_rmZ-VI8ds@S5KkcRx0c81zXc0-gxaYPIQ@S5KkcRx8epgDUJEzxlfUDcg@S5KkcRU8Y9lbecUzykvEDdg@Sv_50RR4Z9lXTIh6b1A',
+  'SZ3_rmZ-VI8ds@S5KkcRx0c81zXc0-gxaYPIQ@S5KkcRx8epgDUJEzxlfUDcg@S5KkcRU8Y9lbecUzykvEDdg@Sv_50RR4Z9lXTIh6b1A',
+  'SZ3_rmZ-VI8ds@S5KkcRx0c81zXc0-gxaYPIQ@S5KkcRx8epgDUJEzxlfUDcg@S5KkcRU8Y9lbecUzykvEDdg@Sv_50RR4Z9lXTIh6b1A',
+  'SZ3_rmZ-VI8ds@S5KkcRx0c81zXc0-gxaYPIQ@S5KkcRx8epgDUJEzxlfUDcg@S5KkcRU8Y9lbecUzykvEDdg@Sv_50RR4Z9lXTIh6b1A',
+  'SZ3_rmZ-VI8ds@S5KkcRx0c81zXc0-gxaYPIQ@S5KkcRx8epgDUJEzxlfUDcg@S5KkcRU8Y9lbecUzykvEDdg@Sv_50RR4Z9lXTIh6b1A',
 ]
 let nowTimes = new Date(new Date().getTime() + new Date().getTimezoneOffset() * 60 * 1000 + 8 * 60 * 60 * 1000);
 !(async () => {
