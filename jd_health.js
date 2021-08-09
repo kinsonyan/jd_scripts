5d937b800983f1e6daec620c752b84e539bdf729
Kinson yan <kinson.yan@outlook.com>
2021-08-09 10:20:30 +0800

    update 20210809

--- jd_health.js
+++ jd_health.js
@@ -7,17 +7,17 @@
 ===================quantumultx================
 [task_local]
 #东东健康社区
-13 1,6,22 * * * jd_health.js, tag=东东健康社区, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
+13 1,6,22 * * * https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_health.js, tag=东东健康社区, img-url=https://raw.githubusercontent.com/Orz-3/mini/master/Color/jd.png, enabled=true
 
 =====================Loon================
 [Script]
-cron "13 1,6,22 * * *" script-path=jd_health.js, tag=东东健康社区
+cron "13 1,6,22 * * *" script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_health.js, tag=东东健康社区
 
 ====================Surge================
-东东健康社区 = type=cron,cronexp="13 1,6,22 * * *",wake-system=1,timeout=3600,script-path=jd_health.js
+东东健康社区 = type=cron,cronexp="13 1,6,22 * * *",wake-system=1,timeout=3600,script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_health.js
 
 ============小火箭=========
-东东健康社区 = type=cron,script-path=jd_health.js, cronexpr="13 1,6,22 * * *", timeout=3600, enable=true
+东东健康社区 = type=cron,script-path=https://raw.githubusercontent.com/Aaron-lv/sync/jd_scripts/jd_health.js, cronexpr="13 1,6,22 * * *", timeout=3600, enable=true
  */
 const $ = new Env("东东健康社区");
 const jdCookieNode = $.isNode() ? require("./jdCookie.js") : "";
@@ -46,7 +46,7 @@ const inviteCodes = [
                     'T012Z3_rmZ-VI8dsCjVfnoaW5kRrbA@T0225KkcRx0c81zXc0-gxaYPIQCjVfnoaW5kRrbA@T0225KkcRx8epgDUJEzxlfUDcgCjVfnoaW5kRrbA@T0225KkcRU8Y9lbecUzykvEDdgCjVfnoaW5kRrbA@T0205KkcF0NMsAiBSUS1xZVtCjVfnoaW5kRrbA@T0205KkcEV9ZvzCxZGye4INrCjVfnoaW5kRrbA@T0205KkcFm1ooAyhUWOS565MCjVfnoaW5kRrbA@T018v_50RR4Z9lXTIh6b1ACjVfnoaW5kRrbA',
                     ]
 let reward = process.env.JD_HEALTH_REWARD_NAME ? process.env.JD_HEALTH_REWARD_NAME : ''
-const randomCount = 0;//const randomCount = $.isNode() ? 20 : 5;
+const randomCount = $.isNode() ? 20 : 5;
 if ($.isNode()) {
   Object.keys(jdCookieNode).forEach((item) => {
     cookiesArr.push(jdCookieNode[item]);
@@ -210,7 +210,7 @@ async function getCommodities() {
       try {
         if (safeGet(data)) {
           data = $.toObj(data)
-          let beans = data.data.result.jBeans.filter(x => x.status !== 0 && x.status !== 1)
+          let beans = data.data.result.jBeans.filter(x => x.status !== 1)
           if (beans.length !== 0) {
             for (let key of Object.keys(beans)) {
               let vo = beans[key]
